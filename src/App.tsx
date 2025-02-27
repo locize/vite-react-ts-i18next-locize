34293875155e761a0f4aa12e796fd289352c8e4d
Adriano Raiano <adriano@raiano.ch>
2025-02-27 15:32:12 +0100

    cosmetics

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,7 @@ function App() {
                 setCount(count + 1)
               }}
             >
-              {lngs[lng].nativeName}
+              {lngs[lng].nativeName.split(',')[0]}
             </button>
           )
         })}
